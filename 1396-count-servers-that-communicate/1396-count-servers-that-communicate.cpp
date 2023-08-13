8ffa12b0dd424100e74b1e9fef318e50a828f751
NandeeshPuri <72340454+NandeeshPuri@users.noreply.github.com>
2023-08-13 19:03:07 +0530

    Time: 46 ms (92.64%), Space: 22.5 MB (36.62%) - LeetHub

--- 1396-count-servers-that-communicate/1396-count-servers-that-communicate.cpp
+++ 1396-count-servers-that-communicate/1396-count-servers-that-communicate.cpp
@@ -1,36 +1,91 @@
+class Disjointset {
+    vector<int> rank, parent, size;
+public:
+ //constructor
+    Disjointset(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+///find ultimate parent function
+    int findUpar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUpar(parent[node]);
+    }
+
+    void unionbyrank(int u, int v) {
+        int ulp_u = findUpar(u);
+        int ulp_v = findUpar(v);
+        if (ulp_u == ulp_v) return;//both belong to same compo as both have same ultimateparent
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {//same rank
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionbysize(int u, int v) {
+        int ulp_u = findUpar(u);
+        int ulp_v = findUpar(v);
+        if (ulp_u == ulp_v) return;//both belong to same compo as both have same ultimateparent
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
 class Solution {
 public:
-
     int countServers(vector<vector<int>>& grid) {
-        int n = grid.size(), m = grid[0].size();
-
-        int ans = 0;
-
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                int val = 0;
+        //step 1 har row col me kitne server hai (using hashing)
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<int>hash(n+m,0);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(grid[i][j]){
-                for(int k = 0; k < n; k++) {
-                    if(grid[k][j] && k != i) {
-                        val = 1; break;
-                    }
+                    hash[i]++;
+                    hash[j+n]++;
                 }
-
-                if(!val) {
-                    for(int k = 0; k < m; k++) {
-                        if(grid[i][k] && k != j) {
-                            val = 1; break;
-                        }
-                    }
-                }}
-
-                cout << val << " ";
-
-                ans += val;
             }
-            cout << endl;
         }
-
-        return ans;
+//nodes are rows number from 0 to n-1 && then columns from 0 to m-1 but both rows and cols nodes value will be same ,so for columns from n to n+m-1
+        //step 2 connecting nodes by dsu
+        Disjointset ds(n+m);
+         for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]){
+                   ds.unionbysize(i,j+n);
+                }
+            }
+        }
+        //step 3  summing up all servers of children of a disjoint  set component
+        map<int,int>mp;
+        for(int i=0;i<n+m;i++){
+            int par=ds.findUpar(i);
+            mp[par]+=hash[i];
+        }
+        //counting servers that can communicate
+        int ans=0;
+        for(auto i:mp){
+           if(i.second>2){
+           ans+=(i.second/2);
+           }
+        }
+return ans;
     }
 };
