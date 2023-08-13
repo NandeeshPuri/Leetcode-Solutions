e00f6eec57668da24dac8d3a3f07558d7df99706
NandeeshPuri <72340454+NandeeshPuri@users.noreply.github.com>
2023-08-13 16:55:19 +0530

    Time: 145 ms (76.31%), Space: 84 MB (85.85%) - LeetHub

--- 2443-check-if-there-is-a-valid-partition-for-the-array/2443-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2443-check-if-there-is-a-valid-partition-for-the-array/2443-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -1,40 +1,17 @@
 class Solution {
 public:
-    bool solveMem(vector<int>&nums,vector<int>&dp,int i){
-        int n=nums.size(); 
-
-        //base case
-        if(i>=n) return true;
-
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        //For equal elements
-        if(i+1<n && nums[i]==nums[i+1]){
-            dp[i]=solveMem(nums,dp,i+2);
-            if(dp[i]) return true;
-
-            if(i+2<n && nums[i]==nums[i+2]){
-                dp[i]=solveMem(nums,dp,i+3);
-                if(dp[i]) return true;
+    bool validPartition(vector<int>& nums) {
+        vector<bool> dp(nums.size()+1);
+        
+        dp[0] = true;
+        for (int i = 2; i <= nums.size(); i++) {
+            dp[i] = dp[i-2] && nums[i-1] == nums[i-2];
+            if (i > 2) {
+                dp[i] = dp[i] || (dp[i-3] && nums[i-1] == nums[i-2] && nums[i-2] == nums[i-3]);
+                dp[i] = dp[i] || (dp[i-3] && nums[i-1] == nums[i-2] + 1 && nums[i-2] == nums[i-3]+1);
             }
         }
-        //For 3 consecutive increasing elements
-        if(i+2<n && nums[i+1]-nums[i]==1 && nums[i+2]-nums[i+1]==1){
-            dp[i]=solveMem(nums,dp,i+3);
-            if(dp[i]) return true;
-        }
-
-        return false;
-    }
-    bool validPartition(vector<int>& nums) {
         
-        int n=nums.size();
-         
-         vector<int>dp(n+1,-1);
-
-         return solveMem(nums,dp,0);
-
-
+        return dp[nums.size()];
     }
 };
