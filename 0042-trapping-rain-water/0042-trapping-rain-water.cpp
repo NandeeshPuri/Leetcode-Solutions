aa9f7e6f25214658825b2df05cd17dceeb20f520
NandeeshPuri <72340454+NandeeshPuri@users.noreply.github.com>
2023-08-16 21:25:58 +0530

    Time: 15 ms (64.17%), Space: 19.8 MB (53.44%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,24 +1,51 @@
 class Solution {
 public:
-    int trap(vector<int>& height)
-    {
-        if(height.empty())
-        return 0;
-        int ans = 0;
-        int size = height.size();
-        vector<int> left_max(size), right_max(size);
-        left_max[0] = height[0];
-        for (int i = 1; i < size; i++) {
-            left_max[i] = max(height[i], left_max[i - 1]);
-        }
-        right_max[size - 1] = height[size - 1];
-        for (int i = size - 2; i >= 0; i--) {
-            right_max[i] = max(height[i], right_max[i + 1]);
-        }
+    
+    int trap(vector<int>& height) {
         
-        for (int i = 1; i < size - 1; i++) {
-            ans += min(left_max[i], right_max[i]) - height[i];
+        // brute force
+        // int ans = 0;
+        // int size = height.size();
+        // for (int i = 1; i < size - 1; i++) {
+        //     int left_max = 0, right_max = 0;
+        //     for (int j = i; j >= 0; j--) { //Search the left part for max bar size
+        //         left_max = max(left_max, height[j]);
+        //     }
+        //     for (int j = i; j < size; j++) { //Search the right part for max bar size
+        //         right_max = max(right_max, height[j]);
+        //     }
+        //     ans += min(left_max, right_max) - height[i];
+        // }
+        // return ans;
+
+        // optimal 
+
+        int n = height.size();
+        int left = 0,right = n-1;
+        int res = 0;
+        int left_max = 0,right_max = 0;
+
+        while(left <= right){
+            if(height[left] <= height[right]){
+                if(height[left] >= left_max){
+                    left_max = height[left];
+                }
+                else{
+                    res += left_max - height[left];
+                }
+                left++;
+            }
+            else{
+                if(height[right] >= right_max){
+                    right_max = height[right];
+                }
+                else{
+                    res += right_max - height[right];
+                }
+                right--;
+            }
         }
-        return ans;
+        return res;
+
     }
 };
