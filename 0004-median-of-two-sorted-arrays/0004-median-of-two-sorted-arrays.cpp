1cd1d0963640e87ff15dce3766a7ab3e2818beae
NandeeshPuri <72340454+NandeeshPuri@users.noreply.github.com>
2023-08-17 12:09:40 +0530

    Time: 25 ms (85.08%), Space: 89.2 MB (75.18%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-    int n = nums1.size();
-    int m = nums2.size();
-    
-    vector<double> res;
-    
-    for(int i=0;i<n;i++){
-        res.push_back(nums1[i]);
-    }
-    
-    for(int i=0;i<m;i++){
-        res.push_back(nums2[i]);
-    }
-    
-    sort(res.begin(),res.end());
-    
-    int k = res.size();
-    double ans = 0;
-    
-    if(k%2 == 0){
-        
-        int idx = k/2;
-        ans = (res[idx] + res[idx-1])/2;
-        
-    }else{
-        int idx = k/2;
-        ans = res[idx];
-    }
-    return ans;
-    
-}
+        if(nums1.size() > nums2.size()) {
+            return findMedianSortedArrays(nums2,nums1);
+        }
 
+        int left = (nums1.size() + nums2.size() + 1) / 2;
+        int low = 0;
+        int high = nums1.size();
+        while(low <= high){
+            int mid1 = (low + high) / 2;
+            int mid2 = left - mid1;
+
+            int L1 = INT_MIN, L2 = INT_MIN;
+            int R1 = INT_MAX, R2 = INT_MAX;
+            if(mid1 - 1 >= 0) L1 = nums1[mid1-1];
+            if(mid2 - 1 >= 0) L2 = nums2[mid2-1];
+            if(mid1 < nums1.size()) R1 = nums1[mid1];
+            if(mid2 < nums2.size()) R2 = nums2[mid2];
+
+            if(L1 <= R2 && L2 <= R1){
+                if((nums1.size() + nums2.size()) % 2 == 0){
+                    return ((double)(max(L1,L2) + min(R1,R2))) / 2.0;    
+                }
+                else{
+                    return max(L1,L2);
+                }
+            }
+            else if(L1 > R2){
+                high = mid1 - 1;
+            }
+            else{
+                low = mid1 + 1;
+            }
+        }
+        return 0.0;
+    }
 };
