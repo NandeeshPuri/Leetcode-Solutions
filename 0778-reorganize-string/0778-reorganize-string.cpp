d54e0e236773b79d7815f10e2645f371af6c8ce2
NandeeshPuri <72340454+NandeeshPuri@users.noreply.github.com>
2023-08-23 22:06:03 +0530

    Time: 3 ms (51.26%), Space: 6.3 MB (21.22%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.cpp
+++ 0778-reorganize-string/0778-reorganize-string.cpp
@@ -1,47 +1,36 @@
-// BRUTE FORCE (using Heap)
-/*
-TC : O(n.logn) // heap -> push(),pop() for n elements
-SC : O(n) // heap
-*/
 class Solution {
 public:
     string reorganizeString(string s) {
-        string ans = "";
-
-        // create map to store characters and their frequencies
-        unordered_map<char,int> map; // SC: O(1)
-        
-        int n = s.length();
-        for(int i = 0; i < n; i++) { // TC: O(n)
-            map[s[i]]++;
+       vector<int>f(26,0);
+       int n=s.size();
+       for(int i=0;i<s.size();i++){
+           f[s[i]-'a']++;
+        if(f[s[i]-'a']>(n+1)/2)
+        return "";
+       }
+       priority_queue<pair<int,char>>p;
+       for(int i=0;i<26;i++){
+        if(f[i]!=0){
+            p.push({f[i],(char)i+'a'});
         }
-
-        priority_queue<pair<int,char>> pq; // max heap of pair type - {freq, char}
-
-        // put characters having (freq > 0) in max heap
-        for(int i = 'a'; i <= 'z'; i++) { // TC: O(1)
-            if(map[i]) {
-                pq.push(make_pair(map[i], i));
-                cout << pq.top().first << " " << pq.top().second << endl;
-            }
-        }
-
-        if(pq.top().first > (n+1)/2) // TC: O(logn)
-            return ans; // ans is empty
-        
-        // add characters in 'ans' string having highest freq which is not selected just before
-        pair<int,char> prev = {-1,'#'};
-        while(!pq.empty()) { // TC: O(n.logn)
-            pair<int,char> curr = pq.top();
-            pq.pop();
-            ans = ans + curr.second;
-            if(prev.first > 0) {
-                pq.push(prev);
-            }
-            curr.first--;
-            prev = curr;
-        }
-
-        return ans;
+       }
+       string ans="";
+       while(p.size()>=2){
+           pair<int,char>p1=p.top();
+           p.pop();
+           pair<int,char>p2=p.top();
+           p.pop();
+           ans+=p1.second;
+           ans+=p2.second;
+           if(p1.first>1){
+           p.push({p1.first-1,p1.second});
+           }
+           if(p2.first>1){
+             p.push({p2.first-1,p2.second});
+           }
+       }
+       if(p.size()==1)
+        ans+=p.top().second;
+       return ans;
     }
 };
