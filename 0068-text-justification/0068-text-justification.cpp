e201ade9be1e7ea060ce1e099d0bdb5094de701a
NandeeshPuri <72340454+NandeeshPuri@users.noreply.github.com>
2023-08-24 19:23:44 +0530

    Time: 4 ms (33.5%), Space: 7.6 MB (16.62%) - LeetHub

--- 0068-text-justification/0068-text-justification.cpp
+++ 0068-text-justification/0068-text-justification.cpp
@@ -1,55 +1,85 @@
 class Solution {
 public:
-    vector<string> fullJustify(vector<string>& words, int maxWidth) {
-        vector<string>res;
-        int n=words.size();
-        int i=0;
-        while(i<n){
-            int j=i+1;
-            int c=words[i].size();
-            int wc=words[i].size();
-            while(j<n&&c+words[j].size()+1<=maxWidth){
-                c+=words[j].size()+1;
-                wc+=words[j].length();
-                j++;
+    vector<string> fullJustify(vector<string>& w, int mw) {
+        int cnt=0,i,cw=0,l=0,n=w.size();
+        vector<string> ans;
+        string t="";
+        
+        for(i=0;i<n;i++){
+            if(cnt+w[i].length()+cw<=mw){
+                cnt+=w[i].length(); // no of alphabets added also increases
+                cw++; // no of words added increases
+                if(i!=(n-1)) // if this is not the last word 
+                    continue;
             }
-           if(j==n||(j-i)==1){
-               string s="";
-               for(int p=i;p<j;p++){
-                   s+=words[p];
-                   if(p!=j-1)
-                   s+=' ';
-               }
-               int ts=maxWidth-s.size();
-               for(int k=0;k<ts;k++){
-                   s+=' ';
-               }
-               res.push_back(s);
-               i=j-1;
-           }
-           else{
-             int tw=j-i;
-             int ts=maxWidth-wc;
-             int es=ts/(tw-1);
-             int extra=ts%(tw-1);
-             string s="";
-             for(int p=i;p<j;p++){
-                 s+=words[p];
-                 if(extra>0){
-                    s+=' ';
-                    extra--;
-                 }
-                 if(p!=j-1){
-                for(int k=0;k<es;k++){
-                   s+=' ';
-               }
-                 }
-             }
-             res.push_back(s);
-              i=j-1;
-           }
-           i++;
+            else
+                i--;
+            
+			// if only 1 word has been added so make it left aligned
+            if(cw==1){
+                string s=w[l];
+                while(s.length()<mw)
+                    s=s+" ";
+                ans.push_back(s);
+                l=i+1;
+                cnt=0,cw=0;
+                continue;
+            }
+            
+			// for cw words we need cw-1 gaps
+            cw--;
+			
+            int spaces=mw-cnt; // total spaces
+            // per - space per gaps
+			// rem - no of gaps on left with 1 extra space
+			int rem=spaces%cw,per=spaces/cw;
+            
+            cnt=0,cw=0;
+            string sp="",s="";
+			
+			// uniform gap
+            while(per--)
+                sp+=" ";
+            
+			// 1st few gaps filled with per+1 spaces
+            while(rem--)
+                s=s+w[l++]+sp+" ";
+            
+			// last gaps which are uniformly filled with "per" spaces
+            while(l<i)
+                s+=w[l++]+sp;
+            
+            s+=w[l++];
+            ans.push_back(s);
+            l=i+1;
         }
-        return res;
+		
+		// making the last string left aligned
+        t=ans[ans.size()-1];
+        ans.pop_back();
+        bool f=1;
+        
+        string s="";
+        for(auto c:t){
+            if(c==' '){
+               if(f){
+                    s=s+c;
+                    f=0;
+                }
+            }
+            else{
+                s=s+c;
+                f=1;
+            }
+        }
+        
+        while(s.length()<mw)
+            s=s+" ";
+        
+        
+        ans.push_back(s);
+        
+        
+        return ans;
     }
 };
