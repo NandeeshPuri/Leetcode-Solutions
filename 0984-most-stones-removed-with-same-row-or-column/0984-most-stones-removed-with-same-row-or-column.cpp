0121fbf7c907da17a9d1195d4f53a717aa234931
NandeeshPuri <72340454+NandeeshPuri@users.noreply.github.com>
2023-08-25 22:02:17 +0530

    Time: 156 ms (36.7%), Space: 14.2 MB (90.94%) - LeetHub

--- 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.cpp
+++ 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.cpp
@@ -1,27 +1,42 @@
 class Solution {
 public:
-    int n;
-    vector<int> vis;
-    //Same as no of ISalands and can be aslo done with union find algorithm (DSU)
-    void dfs(int idx,vector<vector<int>> &stones){
-        vis[idx] = true;
-        for(int i=0;i<n;i++){
-            if(vis[i]) continue;
-            if(stones[idx][0] == stones[i][0] || stones[idx][1] == stones[i][1]){
-                dfs(i,stones);
+    bool isValid(vector<int>&pos1, vector<int>&pos2)
+    {
+        if (pos1[0] == pos2[0]) return true;
+        if (pos1[1] == pos2[1]) return true;
+        return false;
+    }
+    int findParent(vector<int>&parent, int node)
+    {
+        while(parent[node] != node) { node = parent[node]; }
+        return node;
+    }
+    void merge(vector<int>&parent, int& componentCount, int node1, int node2)
+    {
+        int parent1 = findParent(parent, node1);
+        int parent2 = findParent(parent, node2);
+        if (parent1 != parent2) 
+        {
+            componentCount--; //whenever we merge two components, totalComponentCount decrease by 1
+            parent[parent1] = parent2; //or parent[parent2] = parent1 would also work :)
+        }
+    }
+    int removeStones(vector<vector<int>>& stones)
+    {
+        int n = stones.size();
+        vector<int>parent(n);
+        for (int i = 0; i < n; i++) parent[i] = i;
+        //==============================================
+        int componentCount = n; //assuming "n" independent components at start
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i; j < n; j++)
+            {
+                if (isValid(stones[i], stones[j]))
+                    merge(parent, componentCount, i, j);
             }
         }
-        return;
-    }
-    int removeStones(vector<vector<int>>& stones) {
-        n = stones.size();
-        vis.resize(n,0);
-        int val = 0;
-        for(int i=0;i<n;i++){
-            if(vis[i]) continue;
-            val++;
-            dfs(i,stones);
-        }
-        return n - val;
+        //===============================================
+        return (n - componentCount); 
     }
 };
