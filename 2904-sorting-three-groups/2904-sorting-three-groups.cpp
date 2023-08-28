482e5243134b5193b07286a9c4968f6b308c8a49
NandeeshPuri <72340454+NandeeshPuri@users.noreply.github.com>
2023-08-28 22:06:36 +0530

    Time: 74 ms (55.88%), Space: 88.4 MB (62.06%) - LeetHub

--- 2904-sorting-three-groups/2904-sorting-three-groups.cpp
+++ 2904-sorting-three-groups/2904-sorting-three-groups.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
-    int minimumOperations(vector<int>& nums) {
-        int n=nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(3,0));
-        for(int i=1;i<=n;i++){
-            dp[i][0]=(nums[i-1]!=1)+dp[i-1][0];
-            dp[i][1]=(nums[i-1]!=2)+min(dp[i-1][0],dp[i-1][1]);
-            dp[i][2] = (nums[i - 1] != 3) + min({dp[i - 1][0], dp[i - 1][1], dp[i - 1][2]});
+    int minimumOperations(vector<int>& v) {
+        int n=v.size();
+        vector<int>dp(n,1);
+        int m=1;
+        for(int i=1;i<n;i++)
+        {
+            for(int j=i-1;j>=0;j--)
+            {
+                if(v[i]>=v[j])
+                {
+                    dp[i]=max(dp[i],1+dp[j]);
+                    m=max(m,dp[i]);
+                }
+            }
         }
-        return min({dp[n][0],dp[n][1],dp[n][2]});
+        return n-m;
     }
 };
