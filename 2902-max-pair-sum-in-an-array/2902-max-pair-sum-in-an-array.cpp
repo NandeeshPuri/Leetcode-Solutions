d535c59889b25b08c11a963a13595805cad01fb9
NandeeshPuri <72340454+NandeeshPuri@users.noreply.github.com>
2023-09-01 21:39:07 +0530

    Time: 28 ms (84.56%), Space: 71.7 MB (13.72%) - LeetHub

--- 2902-max-pair-sum-in-an-array/2902-max-pair-sum-in-an-array.cpp
+++ 2902-max-pair-sum-in-an-array/2902-max-pair-sum-in-an-array.cpp
@@ -1,34 +1,20 @@
 class Solution {
 public:
     int maxSum(vector<int>& nums) {
-        vector<vector<int>>v(10);
-        int x=0,y=0,z=0;
-        for(auto &i:nums){
-            x=i;y=0;
-            while(x){
-                z=(x%10);
-                y=max(y,z);
-                x=x/10;
-            }
-            v[y].push_back(i);
+    int ans = -1;
+    unordered_map<int,vector<int>> mp;
+    for(auto n: nums){
+        int t = n, maxDigit = 0;
+        while(t){ //evaluate max digit in the number
+            maxDigit = max(t%10, maxDigit);
+            t = t/10;
         }
-
-        int mx1=0,mx2=0,ans=-1;
-        for(auto &i:v){
-            if(i.size()<=1)continue;
-
-            mx1=0,mx2=0;
-            for(auto &j:i){
-                if(j>mx1){
-                    mx2=mx1;
-                    mx1=j;
-                }else if(j>mx2){
-                    mx2=j;
-                }
-            }
-
-        ans=max(mx1+mx2,ans);
-        }
-        return ans;
+        mp[maxDigit].push_back(n); // add the number to the map 
+    }
+    for(auto [k, v]: mp){
+        sort(v.rbegin(), v.rend()); //to find max two number in each max digit 
+        if(v.size() >= 2) ans = max(ans, v[0] + v[1]); //sum max two number and take max
+    }
+    return ans;
     }
 };
