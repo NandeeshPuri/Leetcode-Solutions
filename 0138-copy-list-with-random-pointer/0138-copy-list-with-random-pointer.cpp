d9e99d7f3eddce5c1fd7616ea53cf7e0b7718c09
NandeeshPuri <72340454+NandeeshPuri@users.noreply.github.com>
2023-09-05 20:56:00 +0530

    Time: 7 ms (71.12%), Space: 11.4 MB (15.63%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,48 +17,23 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        if(!head) return nullptr;
-
-        Node* original = head; // itr for original
-        Node* cloned = nullptr; // itr for cloed
-        Node* temp = nullptr; // temporary for node creation
-
-        // First pass: clone nodes and link them next to origial
-        while(original) {
-            temp = new Node(original->val);
-            temp->next = original->next;
-            original->next=temp;
-            original = temp->next;
-        }
-
-        original = head;
-        Node* newHead = head->next;
-
-        // Second pass: Assign random pointers to cloned nodes
-        while(original) {
-            if(original->random) {
-                original->next->random = original->random->next;
-            } else {
-                original->next->random = nullptr;
-            }
-            original = original->next->next; // skip copy
+        if (!head) return nullptr;
+        
+        unordered_map<Node*, Node*> old_to_new;
+        
+        Node* curr = head;
+        while (curr) {
+            old_to_new[curr] = new Node(curr->val);
+            curr = curr->next;
         }
         
-        original = head;
-
-        // Third Pass: Seperate original and cloned 
-        while(original) {
-            temp = original->next; 
-            original->next = temp->next; 
-            original = original->next;
-
-            if(original) {
-                temp->next = original->next;
-            } else {
-                temp->next = nullptr;
-            }
+        curr = head;
+        while (curr) {
+            old_to_new[curr]->next = old_to_new[curr->next];
+            old_to_new[curr]->random = old_to_new[curr->random];
+            curr = curr->next;
         }
-
-    return newHead;
+        
+        return old_to_new[head];
     }
 };
