dba94f7aa53e002ac655bc12e3cd40d1efa03a71
NandeeshPuri <72340454+NandeeshPuri@users.noreply.github.com>
2023-09-10 22:35:34 +0530

    Time: 4 ms (77.17%), Space: 6.7 MB (21.36%) - LeetHub

--- 2886-faulty-keyboard/2886-faulty-keyboard.cpp
+++ 2886-faulty-keyboard/2886-faulty-keyboard.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     string finalString(string s) {
-        string res="";
-        for(auto c:s){
-            if(c=='i'){
-                if(res.size()){
-                    reverse(res.begin(),res.end());
-                }
-            }
-            else{
-                res+=c;
-            }
+        string ans;
+        for(char c : s)
+        {
+            if(c=='i') reverse(ans.begin(),ans.end());
+            else ans+=c;
         }
-        return res;
+        return ans;
+
     }
 };
