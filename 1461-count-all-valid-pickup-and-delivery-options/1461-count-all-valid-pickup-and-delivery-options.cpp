0fb2702a24642a35247fc21d17318b3f94a57c63
NandeeshPuri <72340454+NandeeshPuri@users.noreply.github.com>
2023-09-10 16:16:12 +0530

    Time: 2 ms (56.67%), Space: 6 MB (43.33%) - LeetHub

--- 1461-count-all-valid-pickup-and-delivery-options/1461-count-all-valid-pickup-and-delivery-options.cpp
+++ 1461-count-all-valid-pickup-and-delivery-options/1461-count-all-valid-pickup-and-delivery-options.cpp
@@ -1,20 +1,11 @@
 class Solution {
+const int MOD = 1e9 + 7;
 public:
-    const int MOD = 1e9 + 7;  // Modulus for calculations
-    vector<long long> dp;    // Array for memoization
-
-    int countOrders(int numberOfPairs) {
-        dp.resize(numberOfPairs + 1); // Initialize memoization array
-
-        // Base case: there is one way to arrange 0 pairs
-        dp[0] = 1;
-
-        // Iterate over all values of pairs from 1 to n
-        for (int currentPairs = 1; currentPairs <= numberOfPairs; currentPairs++) {
-            // Calculate the number of valid orders for the current number of pairs
-            dp[currentPairs] = dp[currentPairs - 1] * (2 * currentPairs - 1) * currentPairs % MOD;
+    int countOrders(int n) {
+        long long count = 1;
+        for (int i = 2; i <= n; ++i) {
+            count = (count * (2 * i - 1) * i) % MOD;
         }
-
-        return dp[numberOfPairs]; // Return the result for n pairs
+        return (int)count;
     }
 };
