8c423cabe15deb716d896a25dd7f351a13e39e78
NandeeshPuri <72340454+NandeeshPuri@users.noreply.github.com>
2023-09-10 22:35:54 +0530

    Time: 8 ms (37.89%), Space: 6.5 MB (41.25%) - LeetHub

--- 2886-faulty-keyboard/2886-faulty-keyboard.cpp
+++ 2886-faulty-keyboard/2886-faulty-keyboard.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string finalString(string s) {
         string ans;
-        for(char c : s)
+        for(auto c : s)
         {
             if(c=='i') reverse(ans.begin(),ans.end());
             else ans+=c;
