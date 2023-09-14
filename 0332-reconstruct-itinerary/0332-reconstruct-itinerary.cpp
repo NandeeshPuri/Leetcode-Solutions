ee81a41fb7cc54828b6684ced73412fd97068ca0
NandeeshPuri <72340454+NandeeshPuri@users.noreply.github.com>
2023-09-14 21:45:01 +0530

    Time: 15 ms (89.66%), Space: 14.5 MB (58.51%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
@@ -1,44 +1,34 @@
 class Solution {
-private:
-    int num_;
-    vector<string> ans_;
-    unordered_map<string, vector<int>> graph_;
-    bool dfs(vector<vector<string>> &tickets, const string &start, vector<bool> &visited, vector<string> &ans) {
-        if (ans.size() == num_ + 1) {
-            ans_ = ans;
-            return true;
+    // Depth-first search function to find the itinerary
+    void dfs(unordered_map<string, multiset<string>>& adj, vector<string>& result, string s){
+        // While there are destinations connected to the current airport
+        while(adj[s].size()){
+            // Get the next destination
+            string v = *(adj[s].begin());
+            // Remove this destination from the list
+            adj[s].erase(adj[s].begin());
+            // Recursively explore this destination
+            dfs(adj, result, v);
         }
-        for (const int edge_id : graph_[start]) {
-            if (!visited[edge_id]) {
-                visited[edge_id] = true;
-                const string &destin = tickets[edge_id][1];
-                ans.push_back(destin);
-                if (dfs(tickets, destin, visited, ans)) return true;
-                ans.pop_back();
-                visited[edge_id] = false;
-            }
-        }
-        return false;
+        // Add the current airport to the result
+        result.push_back(s);
     }
-
 public:
+    // Main function to find the itinerary
     vector<string> findItinerary(vector<vector<string>>& tickets) {
-        num_ = tickets.size();
-        // start point to edge idx
-        for (int i = 0; i < num_; i++) {
-            const auto &ticket = tickets[i];
-            graph_[ticket[0]].push_back(i);
-        }
-        // sort the edges based on the string
-        for (auto &p : graph_) {
-            auto &edges = p.second;
-            sort(edges.begin(), edges.end(), [&](int a, int b) {
-            return tickets[a][1] < tickets[b][1];
-        });
-        }
-        vector<string> ans = {"JFK"};
-        vector<bool> visited(num_, false);
-        dfs(tickets, "JFK", visited, ans);
-        return ans_;
+        // Create an adjacency list to represent the flights
+        unordered_map<string, multiset<string>> adj;
+        for(vector<string>& t: tickets)
+            // Add each destination to the multiset connected to its source airport
+            adj[t[0]].insert(t[1]);
+        
+        // Initialize the result vector
+        vector<string> result;
+        // Start the depth-first search from JFK (John F. Kennedy International Airport)
+        dfs(adj, result, "JFK");
+        // Reverse the result to get the correct itinerary
+        reverse(result.begin(), result.end());
+        // Return the itinerary
+        return result;
     }
 };
