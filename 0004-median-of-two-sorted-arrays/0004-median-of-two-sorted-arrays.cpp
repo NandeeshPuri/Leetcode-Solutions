1c4b3b192b99d5964381355e4a578ef2914869a9
NandeeshPuri <72340454+NandeeshPuri@users.noreply.github.com>
2023-09-21 18:17:38 +0530

    Time: 35 ms (25.48%), Space: 89.8 MB (37.84%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,39 +1,49 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if(nums1.size() > nums2.size()) {
-            return findMedianSortedArrays(nums2,nums1);
-        }
-
-        int left = (nums1.size() + nums2.size() + 1) / 2;
-        int low = 0;
-        int high = nums1.size();
-        while(low <= high){
-            int mid1 = (low + high) / 2;
-            int mid2 = left - mid1;
-
-            int L1 = INT_MIN, L2 = INT_MIN;
-            int R1 = INT_MAX, R2 = INT_MAX;
-            if(mid1 - 1 >= 0) L1 = nums1[mid1-1];
-            if(mid2 - 1 >= 0) L2 = nums2[mid2-1];
-            if(mid1 < nums1.size()) R1 = nums1[mid1];
-            if(mid2 < nums2.size()) R2 = nums2[mid2];
-
-            if(L1 <= R2 && L2 <= R1){
-                if((nums1.size() + nums2.size()) % 2 == 0){
-                    return ((double)(max(L1,L2) + min(R1,R2))) / 2.0;    
-                }
-                else{
-                    return max(L1,L2);
-                }
+    double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
+        int n1 = nums1.size(), n2 = nums2.size();
+        
+        // Ensure nums1 is the smaller array for simplicity
+        if (n1 > n2)
+            return findMedianSortedArrays(nums2, nums1);
+        
+        int n = n1 + n2;
+        int left = (n1 + n2 + 1) / 2; // Calculate the left partition size
+        int low = 0, high = n1;
+        
+        while (low <= high) {
+            int mid1 = (low + high) >> 1; // Calculate mid index for nums1
+            int mid2 = left - mid1; // Calculate mid index for nums2
+            
+            int l1 = INT_MIN, l2 = INT_MIN, r1 = INT_MAX, r2 = INT_MAX;
+            
+            // Determine values of l1, l2, r1, and r2
+            if (mid1 < n1)
+                r1 = nums1[mid1];
+            if (mid2 < n2)
+                r2 = nums2[mid2];
+            if (mid1 - 1 >= 0)
+                l1 = nums1[mid1 - 1];
+            if (mid2 - 1 >= 0)
+                l2 = nums2[mid2 - 1];
+            
+            if (l1 <= r2 && l2 <= r1) {
+                // The partition is correct, we found the median
+                if (n % 2 == 1)
+                    return max(l1, l2);
+                else
+                    return ((double)(max(l1, l2) + min(r1, r2))) / 2.0;
             }
-            else if(L1 > R2){
+            else if (l1 > r2) {
+                // Move towards the left side of nums1
                 high = mid1 - 1;
             }
-            else{
+            else {
+                // Move towards the right side of nums1
                 low = mid1 + 1;
             }
         }
-        return 0.0;
+        
+        return 0; // If the code reaches here, the input arrays were not sorted.
     }
 };
