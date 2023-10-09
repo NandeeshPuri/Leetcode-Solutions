97b5fe3c878687706f3727997567993e14ed1114
NandeeshPuri <72340454+NandeeshPuri@users.noreply.github.com>
2023-10-09 18:03:12 +0530

    Time: 11 ms (9.25%), Space: 14.1 MB (14.74%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,38 +1,24 @@
 class Solution {
-public:
+  public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int left = binarySearch(nums, target, true);
-        int right = binarySearch(nums, target, false);
+        int a=0,b=nums.size()-1;
+        if(nums.empty()) return {-1,-1} ;
+        if(nums[b]<target||nums[a]>target) return {-1,-1} ;
         
-        if (left <= right) {
-            return {left, right};
-        } else {
-            return {-1, -1};
+        while(a<=b&&b>=0){
+            
+            if(nums[a]==target&&nums[b]==target)
+                return {a,b};
+            if(nums[a]<target) a++;
+            if(a==b){
+                if(nums[a]==target)
+                    return{a,a};
+                else
+                    break;
+            }                
+            if(nums[b]>target) b--;
+                  
         }
-    }
-
-private:
-    int binarySearch(vector<int>& nums, int target, bool findLeft) {
-        int low = 0;
-        int high = nums.size() - 1;
-        int index = -1;
-        
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] == target) {
-                index = mid;
-                if (findLeft) {
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
-                }
-            } else if (nums[mid] < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
-        
-        return index;
+        return {-1,-1} ;
     }
 };
