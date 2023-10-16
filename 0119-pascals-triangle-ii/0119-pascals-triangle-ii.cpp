f29539844897270c6cb5cd618f16190a72bba0e2
NandeeshPuri <72340454+NandeeshPuri@users.noreply.github.com>
2023-10-16 16:45:17 +0530

    Time: 3 ms (36.93%), Space: 6.7 MB (35.86%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-    vector<int> getRow(int rowIndex) {
-        vector<int> arr(rowIndex+1, 1);
-        int next = 1;
-        int prev = next;
-        for(int j=1; j<rowIndex; j++){
-            for(int i=1; i<=j; i++){
-                next = arr[i];
-                arr[i] = arr[i] + prev;
-                prev = next; 
-            }
+    std::vector<int> getRow(int rowIndex) {
+        std::vector<int> result;
+        
+        // Initialize the first element of the row to 1.
+        result.push_back(1);
+        
+        // Calculate the `k`-th row using binomial coefficient formula.
+        for (int i = 1; i <= rowIndex; i++) {
+            // Use the previous element to calculate the current element.
+            long long currentElement = static_cast<long long>(result[i - 1]) * (rowIndex - i + 1) / i;
+            result.push_back(static_cast<int>(currentElement));
         }
-        return(arr);
+        
+        return result;
     }
 };
