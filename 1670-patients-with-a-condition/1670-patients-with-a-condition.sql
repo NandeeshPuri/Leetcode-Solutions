0323e987a9888ed93ec3f809177a5de23aae9a4f
NandeeshPuri <72340454+NandeeshPuri@users.noreply.github.com>
2023-11-01 10:43:28 +0530

    Time: 566 ms (91.45%), Space: 0B (100%) - LeetHub

--- 1670-patients-with-a-condition/1670-patients-with-a-condition.sql
+++ 1670-patients-with-a-condition/1670-patients-with-a-condition.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-SELECT patient_id,patient_name,conditions
+SELECT * 
 FROM patients
-WHERE conditions LIKE '% DIAB1%' OR 
-      conditions LIKE 'DIAB1%'
+WHERE conditions REGEXP '\\bDIAB1'
+#\b in a normal string means 'backspace' but in regular expression it's a boundary word and we want \b to work as a boundary word NOT AS A backspace, so use \ before \b (\\b) to work it as a special character in regular expression. (This same theory works for MySQL as well)
